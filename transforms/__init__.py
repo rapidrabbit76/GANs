6ce44e39f0896bfb3db269dcd69bfd2998b17405
yslee <yslee.dev@gmail.com>
2022-04-19 22:39:25 +0900

    feat: celeba transforms

--- transforms/__init__.py
+++ transforms/__init__.py
@@ -1,9 +1,11 @@
 from typing import Dict, Callable
 from .base import BaseTransforms
+from .celeba import CelebATransforms
 
 
 TRANSFORMS_TABLE: Dict["str", Callable] = {
     "BASE": BaseTransforms,
+    "CelebA": CelebATransforms,
 }
 TRANSFORMS_LIST = list(TRANSFORMS_TABLE.keys())
 
